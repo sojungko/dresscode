e10cfec9c17a4f13cf6d0c9b8be8e720c0b93eb9
Sojung Park <sojungko@gmail.com>
2017-03-09 16:50:12 -0500

    Condense server

--- src/server/main.js
+++ src/server/main.js
@@ -4,20 +4,18 @@ const routes = require('./routes');
 
 const server = new Hapi.Server(3000);
 
-server.register(Blipp, () => {
-  server.route(routes);
-});
-
 server.register([{ // register all your plugins
   register: require('hapi-postgres-connection'),
 },
   Blipp,
 ], (err) => {
   if (err) {
-    // handle plugin startup error
     console.log('Error : ', err);
     throw err;
   }
+
+  server.route(routes);
+
   server.start(() => {
     console.log(`Server running at ${server.info.uri}`);
   });
