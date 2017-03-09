557511368aee5c3434206e275da291fc615ed151
Sojung Park <sojungko@gmail.com>
2017-03-08 19:45:05 -0500

    Set up hapi server

--- src/server/main.js
+++ src/server/main.js
@@ -1,27 +1,37 @@
 const Hapi = require('hapi');
-const server = new Hapi.Server(3000);
+const Inert = require('inert');
+const Blipp = require('blipp');
 
-server.register({
-  register: require('hapi-postgres-connection')
-}, function (err) {
-  if (err) {
-    // handle plugin startup error 
-  }
+require('dotenv').config();
+
+const server = new Hapi.Server();
+server.connection({ port: 8000, host: 'localhost' });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    const email = 'test@test.net';
+    const select = `SELECT * FROM people WHERE ${email}`;
+    request.pg.client.query(select, (err, result) => {
+      console.log(err, result);
+      return reply(result.rows[0]);
+    });
+  },
 });
 
-server.register(Blipp, (err) => {
-  server.route([
-      {
-          method: 'GET',
-          path: '/api/user',
-          handler: function (request, reply) {
-
-              return reply(null);
-          }
-      },
-
-})
-
-server.start(function() {
-    console.log('Hapi is listening to http://localhost:3000');
+server.register([{ // register all your plugins
+  register: require('hapi-postgres-connection'),
+},
+  Inert,
+  Blipp,
+], (err) => {
+  if (err) {
+    // handle plugin startup error
+    console.log('Error : ', err);
+    throw err;
+  }
+  server.start(() => {
+    console.log(`Server running at ${server.info.uri}`);
+  });
 });
