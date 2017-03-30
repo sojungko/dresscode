a470343cbd30c748d47b74cb2143b5b334fad525
Sojung Park <sojungko@gmail.com>
2017-03-30 18:06:26 -0400

    Attempt implementing S3 image upload WIR

--- src/client/actions/index.native.js
+++ src/client/actions/index.native.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import { Actions } from 'react-native-router-flux';
+
 import * as C from '../constants/index.native';
 
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-west-1',
+});
 
 /* -- Image Handling -- */
 export const selectProfilePic = (image) => {
   return { type: C.SELECT_PROFILE_PIC, payload: image };
 };
 
-// 
 export const postProfilePic = (image) => {
   console.log('Posting profile pic... ', image);
-  return dispatch => axios.post('/api/getlocations', image)
-    .then(({ data }) => {
-      console.log('actions/postProfilePic data : ', data);
-
-      return dispatch({ type: C.POST_PROFILE_PIC_SUCCESS, payload: data });
-    });
+  s3.upload();
 };
 
